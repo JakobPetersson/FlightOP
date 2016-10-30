1c378b6d24ca506abfed8db7d9b961432869c728
Jakob Petersson <github@jakobpetersson.se>
2016-10-30 18:43:13 +0100

    Structured code layout.

--- js/BoardingPass/GeneralConditionaItems/GeneralConditionalItems.js
+++ js/BoardingPass/GeneralConditionaItems/GeneralConditionalItems.js
@@ -14,27 +14,27 @@ import SecondNonConsecutiveBaggageTag from "./SecondNonConsecutiveBaggageTag";
 
 class GeneralConditionalItems {
     constructor(provider) {
-        this.beginningOfVersionNumber = new BeginningOfVersionNumber(provider);
-        this.versionNumber = new VersionNumber(provider);
+        this.beginningOfVersionNumber              = new BeginningOfVersionNumber(provider);
+        this.versionNumber                         = new VersionNumber(provider);
 
         this.sizeHex = provider.getData(2);
         this.sizeDec = parseInt(this.sizeHex, 16);
-        this.sizeOfConditional          = new ConditionalItem(0, 'Size of conditional items',      this.sizeHex, 2);
+        this.sizeOfConditional                     = new ConditionalItem(0, 'Size of conditional items',      this.sizeHex, 2);
 
         if (!provider.hasData(this.sizeDec)) {
             console.log(new Error("Not enough data for GeneralConditionalItems"));
         }
 
         let conditionalData = new DataProvider(provider.getData(this.sizeDec));
-        this.passengerDescription = new PassengerDescription(conditionalData);
-        this.sourceOfCheckIn = new SourceOfCheckIn(conditionalData);
-        this.sourceOfBoardingPassIssuance = new SourceOfBoardingPassIssuance(conditionalData);
-        this.dateOfIssueOfBoardingPass = new DateOfIssueOfBoardingPass(conditionalData);
-        this.documentType = new DocumentType(conditionalData);
+        this.passengerDescription                  = new PassengerDescription(conditionalData);
+        this.sourceOfCheckIn                       = new SourceOfCheckIn(conditionalData);
+        this.sourceOfBoardingPassIssuance          = new SourceOfBoardingPassIssuance(conditionalData);
+        this.dateOfIssueOfBoardingPass             = new DateOfIssueOfBoardingPass(conditionalData);
+        this.documentType                          = new DocumentType(conditionalData);
         this.airlineDesignatorOfBoardingPassIssuer = new AirlineDesignatorOfBoardingPassIssuer(conditionalData);
-        this.baggageTag = new BaggageTag(conditionalData);
-        this.firstNonConsecutiveBaggageTag = new FirstNonConsecutiveBaggageTag(conditionalData);
-        this.secondNonConsecutiveBaggageTag = new SecondNonConsecutiveBaggageTag(conditionalData);
+        this.baggageTag                            = new BaggageTag(conditionalData);
+        this.firstNonConsecutiveBaggageTag         = new FirstNonConsecutiveBaggageTag(conditionalData);
+        this.secondNonConsecutiveBaggageTag        = new SecondNonConsecutiveBaggageTag(conditionalData);
 
         this.totalSize = this.totalSize.bind(this);
     }
