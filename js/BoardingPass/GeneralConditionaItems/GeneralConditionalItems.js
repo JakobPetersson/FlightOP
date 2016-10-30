141e28dd365bb5c782b7c885fcc27cd897142b4c
Jakob Petersson <github@jakobpetersson.se>
2016-10-30 18:28:03 +0100

    Created class for SourceOfBoardingPassIssuance

--- js/BoardingPass/GeneralConditionaItems/GeneralConditionalItems.js
+++ js/BoardingPass/GeneralConditionaItems/GeneralConditionalItems.js
@@ -4,6 +4,7 @@ import BeginningOfVersionNumber from "./BeginningOfVersionNumber";
 import VersionNumber from "./VersionNumber";
 import PassengerDescription from "./PassengerDescription";
 import SourceOfCheckIn from "./SourceOfCheckIn";
+import SourceOfBoardingPassIssuance from "./SourceOfBoardingPassIssuance";
 
 class GeneralConditionalItems {
     constructor(provider) {
@@ -21,7 +22,7 @@ class GeneralConditionalItems {
         let conditionalData = new DataProvider(provider.getData(this.sizeDec));
         this.passengerDescription = new PassengerDescription(conditionalData);
         this.sourceOfCheckIn = new SourceOfCheckIn(conditionalData);
-        this.sourceOfBoardingPassIssuance           = new ConditionalItem(0, 'Source of Boarding Pass Issuance',                       conditionalData.getData(1), 1);
+        this.sourceOfBoardingPassIssuance = new SourceOfBoardingPassIssuance(conditionalData);
         this.dateOfIssueOfBoardingPass              = new ConditionalItem(0, 'Date of Issue of Boarding Pass',                         conditionalData.getData(4), 4);
         this.documentType                           = new ConditionalItem(0, 'Document Type',                                          conditionalData.getData(1), 4);
         this.airlineDesignatorOfBoardingPassIssuer  = new ConditionalItem(0, 'Airline Designator of boarding pass issuer',             conditionalData.getData(3), 3);
---
+++ js/BoardingPass/GeneralConditionaItems/SourceOfBoardingPassIssuance.js
@@ -0,0 +1,9 @@
+import Item from "../Item";
+
+class SourceOfBoardingPassIssuance extends Item {
+    constructor(provider) {
+        super(14, 'Source of Boarding Pass Issuance', provider.getData(1), 1);
+    }
+}
+
+export default SourceOfBoardingPassIssuance;
