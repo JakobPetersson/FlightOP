43f26d3e64eb684aeb82bf27635a6d43e14e7f03
Jakob Petersson <github@jakobpetersson.se>
2016-10-30 19:11:44 +0100

    Corrected name for DateOfIssueOfBoardingPass

--- js/BoardingPass/GeneralConditionaItems/DateOfIssueOfBoardingPass.js
+++ js/BoardingPass/GeneralConditionaItems/DateOfIssueOfBoardingPass.js
@@ -2,7 +2,7 @@ import Item from "../Item";
 
 class DateOfIssueOfBoardingPass extends Item {
     constructor(provider) {
-        super(22, 'Source of Boarding Pass Issuance', provider.getData(4), 4);
+        super(22, 'Date of Issue of Boarding Pass', provider.getData(4), 4);
     }
 }
 
